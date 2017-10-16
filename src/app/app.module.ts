cfac23bb6f59e5e2cc009b07f4270234ca146f1d
Tijs Fertinel <tijs@MacBook-Air-van-Tijs.local>
2017-10-16 11:49:58 +0200

    added navigation UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavTopComponent } from './ui/nav-top/nav-top.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavTopComponent
   ],
   imports: [
     BrowserModule
