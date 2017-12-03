4486dd85de6bfa94458f8fe6a9531a2420bcb166
tijs <tijs.fertinel.u8681@student.hogent.be>
2017-12-03 10:30:37 +0100

    nuke git hub, start again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavTopComponent } from './ui/nav-top/nav-top.component';
 
 @NgModule({
   declarations: [
